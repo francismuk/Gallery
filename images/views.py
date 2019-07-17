f24992cd5e99e99ae39046f6302528380137de27
Francis Mukuha <mukuha58@gmail.com>
2019-07-17 16:26:11 +0300

    add single image function

--- images/views.py
+++ images/views.py
@@ -9,3 +9,28 @@ def index(request):
     images = Image.get_all_images()
     return render(request,'index.html', {'title':title,'images':images})
 
+def single_image(request, category, image_id):
+    locations = Location.objects.all()
+    
+    image = Image.get_image_by_id(image_id)
+    images_category = Image.objects.filter(category__photo_category = category_name)
+    title = f'{category_name}'
+    return render(request,'single_image.html',{'title':title, 'image':image, 'image_category':image_category, 'locations':locations})
+
+def location_filter(request, location):
+    locations = Location.objects.all()
+    images = Image.filter_by_location(location)
+    title = f'{location} Photos'
+    return render(request, 'locations.html', {'title':title, 'images':images, 'locations':locations})
+
+def search(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        images_found = Image.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'images':images_found, 'locations':locations})
+    else:
+        message = "Please enter a search object"
+        return render(request, 'search.html',{'message':message, 'locations':locations})
