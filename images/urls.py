7d312ecd4b4def6ad8a43575c3a74fe978341955
Francis Mukuha <mukuha58@gmail.com>
2019-07-18 10:33:32 +0300

    add locations urls

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name='index'),
-
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art'),
+    url(r'^location/(?P<location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^search', views.search, name='search_images')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
