7313116119a164e394621e02b8df6c2594eb285f
Francis Mukuha <mukuha58@gmail.com>
2019-07-20 10:31:55 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -107,14 +107,14 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
-        'USER': 'francis',
-        'PASSWORD': '1234',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gallery',
+#         'USER': 'francis',
+#         'PASSWORD': '1234',
+#     }
+# }
 
 
 # Password validation
